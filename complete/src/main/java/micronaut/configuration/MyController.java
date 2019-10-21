8ff1bfbf869fff8223a88720db2d211cec2d55bb
niravassar <assarn@objectcomputing.com>
2019-10-21 07:01:52 -0500

    change paths and source code references

---
+++ complete/src/main/java/micronaut/configuration/MyController.java
@@ -0,0 +1,32 @@
+package micronaut.configuration
+
+import groovy.transform.CompileStatic
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+import javax.annotation.Nullable
+import javax.inject.Named
+
+@CompileStatic
+@Controller("/my")
+class MyController {
+
+    TeamConfiguration teamConfiguration
+    StadiumConfiguration stadiumConfiguration
+
+    MyController(@Nullable TeamConfiguration teamConfiguration, @Nullable @Named("pnc") StadiumConfiguration stadiumConfiguration) { // <1>
+        this.teamConfiguration = teamConfiguration
+        this.stadiumConfiguration = stadiumConfiguration
+    }
+
+    @Get("/team")
+    HttpResponse<TeamConfiguration> team() {
+        return HttpResponse.ok(this.teamConfiguration)
+    }
+
+    @Get("/stadium")
+    HttpResponse<StadiumConfiguration> stadium() {
+        return HttpResponse.ok(this.stadiumConfiguration)
+    }
+}
---
+++ complete/src/main/java/micronaut/configuration/TeamAdmin.java
@@ -0,0 +1,44 @@
+package micronaut.configuration
+
+class TeamAdmin { // <1>
+
+    String manager, coach, president
+
+    // should use the builder pattern to create the object
+    private TeamAdmin() {
+    }
+
+    static Builder builder() {
+        return new Builder()
+    }
+
+    static class Builder { // <2>
+        String manager
+        String coach
+        String president
+
+        // <3>
+        Builder withManager(String manager) {
+            this.manager = manager
+            this
+        }
+
+        Builder withCoach(String coach) {
+            this.coach = coach
+            this
+        }
+
+        Builder withPresident(String president) {
+            this.president = president
+            this
+        }
+
+        TeamAdmin build() { // <4>
+            TeamAdmin teamAdmin = new TeamAdmin()
+            teamAdmin.manager = this.manager
+            teamAdmin.coach = this.coach
+            teamAdmin.president = this.president
+            teamAdmin
+        }
+    }
+}
